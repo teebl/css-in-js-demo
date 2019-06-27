5a96056729d2cc643b01e706e375d437bc202a07
teebl <trevor.seibel@gmail.com>
2019-06-27 01:06:55 -0400

    conversion to styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+
+import styled, { keyframes } from "styled-components";
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledApp = styled.div`
+  text-align: center;
+`;
+const StyledLogo = styled.img`
+  animation: ${appLogoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const StyledLink = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <StyledApp>
+      <StyledHeader>
+        <StyledLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <StyledLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </StyledLink>
+      </StyledHeader>
+    </StyledApp>
   );
 }
 
