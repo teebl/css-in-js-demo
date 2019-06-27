f4eb8904ceb8afcc58c56a588292c4743f9b309e
teebl <trevor.seibel@gmail.com>
2019-06-27 03:29:53 -0400

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import serializer from "jest-emotion";
+expect.addSnapshotSerializer(serializer);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders snapshot", () => {
+  const tree = renderer.create(<App />).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
